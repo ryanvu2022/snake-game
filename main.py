6a5b890125c021d99947d61253bb75056a2b96d5
Thinh <ryanvu2022@gmail.com>
2022-11-11 18:45:37 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,50 @@
+import time
+from snake import Snake
+from turtle import Screen
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.right, "Right")
+screen.onkey(snake.left, "Left")
+
+is_game_on = True
+while is_game_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
